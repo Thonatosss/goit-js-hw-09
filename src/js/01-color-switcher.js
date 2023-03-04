a10e18422964b25905a3af6db3530f046d1a6232
Mykola Romashenko <sven4369@gmail.com>
2023-03-04 10:31:57 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
+
+startBtn.addEventListener('click', OnStartBtnClick);
+stopBtn.addEventListener('click', OnStopBtnClick);
+
+
+
+function OnStartBtnClick (){
+
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+}
+
+function OnStopBtnClick (){
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+}
 
