858d8f16458ebf5e023d900a3761ffb6a41aa021
Mykola Romashenko <sven4369@gmail.com>
2023-03-07 13:08:37 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('button');
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  for(let i = 0; form[2].value; i += 1){
+    const promise = new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve()
+      } else {
+        reject()
+      }
+    });
   }
+  
 }
