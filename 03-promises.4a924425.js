251f299cf27ccd72ffa4a922b2f397822d1fed4f
Thonatosss <115006947+Thonatosss@users.noreply.github.com>
2023-03-07 11:09:26 +0000

    Deploying to gh-pages from @ Thonatosss/goit-js-hw-09@858d8f16458ebf5e023d900a3761ffb6a41aa021 🚀

---
+++ 03-promises.4a924425.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector("button");
+//# sourceMappingURL=03-promises.4a924425.js.map
---
+++ 03-promises.819cc754.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector("button");
+//# sourceMappingURL=03-promises.819cc754.js.map
