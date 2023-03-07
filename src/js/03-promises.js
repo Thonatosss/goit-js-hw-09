037204addd2bce237e57adb3af4e8d13f7154659
Mykola Romashenko <sven4369@gmail.com>
2023-03-07 22:57:23 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,34 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const form = document.querySelector('.form');
-const submitBtn = document.querySelector('button');
-
-
-
-
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  for(let i = 0; form[2].value; i += 1){
-    const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve()
+        resolve({ position, delay });
       } else {
-        reject()
+        reject({ position, delay });
       }
-    });
-  }
-  
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  for (let i = 0; i < Number(amount.value); i++) {
+    createPromise(i + 1, Number(delay.value) + (Number(step.value) * i))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
